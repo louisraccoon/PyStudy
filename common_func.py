2555a19db056fd6d0eea6d34739e57fe31976ff2
happydog825 <happydog825@gmail.com>
2015-05-21 13:57:45 +0900

    mod 디렉토리 검색 함수 알고리즘  버그 수정

--- common_func.py
+++ common_func.py
@@ -3,8 +3,10 @@ import os
 def getDirectoryList():
     dirList = []
     for root, dirs, files in os.walk('./mod/'):
+        num_sep_this = root.count(os.path.sep)
         for dir in dirs:
             if '__pycache__' != dir:#except folder
                 dirList.append(dir)
+        break
     return dirList
 
