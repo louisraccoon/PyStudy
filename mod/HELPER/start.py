16658290f5bf46b2ff3b22e3d623d8d3a5a99198
happydog825 <happydog825@gmail.com>
2015-05-21 14:02:50 +0900

    불완전 작동 소스 임시 제거

--- mod/HELPER/start.py
+++ mod/HELPER/start.py
@@ -1,3 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication
 
+from common_module_lib import *
+
+def initModule(systemMessageBox, myMessageBox):
+    myModule = Module(systemMessageBox, myMessageBox)
+    mainfunction(myModule)
+def mainfunction(system):
+    print("------Helper process start -------")
+    while 1:
+        message = system.receiveMessage()
+        print("[To Helper]:"+message)
+        system.sendMessage("/Helper how are you?")
+        system.printMessage("Helper receive:"+message)
