9ce95f13a53db5045a25db75a163ed464210a775
happydog825 <happydog825@gmail.com>
2015-05-29 18:30:26 +0900

    1. 모듈 설명 추가

--- common_module_lib.py
+++ common_module_lib.py
@@ -5,18 +5,18 @@ class Module:
     def __init__(self, systemqueue, myqueue):
         self.systemqueue = systemqueue
         self.myqueue = myqueue
-    def sendMessage(self,message):
+    def sendMessage(self,message):# 메시지 보내기
         if message is not None and message != '':
             self.systemqueue.put(message)
-    def printMessage(self,message):
+    def printMessage(self,message):#콘솔창 메시지 보내기
         if message is not None and message != '':
             self.systemqueue.put("/print "+message)
-    def checkMessageEmpty(self):
+    def checkMessageEmpty(self):#시스템으로부터 메시지 있는지 체크
         if self.myqueue.empty():
             return True
         else:
             return False
-    def receiveMessage(self):
+    def receiveMessage(self):#메시지 받기(받을때까지 기다린다.)
         while self.myqueue.empty():
             time.sleep(0.2)
         message = self.myqueue.get()
